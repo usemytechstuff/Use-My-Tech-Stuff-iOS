1316d8fbfe17efb1a8183faaa33cfc19586046b1
Michael Redig <mredig@gmail.com>
2019-05-20 11:45:36 -0500

    added initial project and gitignore

---
+++ Use My Tech Stuff/Use My Tech Stuff/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Use My Tech Stuff
+//
+//  Created by Michael Redig on 5/20/19.
+//  Copyright © 2019 Red_Egg Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
