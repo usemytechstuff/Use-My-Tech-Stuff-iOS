eb45c9c564c85508440806bb0ebe63f810c1f3a2
Michael Redig <mredig@gmail.com>
2019-05-20 13:53:49 -0500

    started working on login ui code

--- Use My Tech Stuff/Use My Tech Stuff/View Controllers/LoginViewController.swift
+++ Use My Tech Stuff/Use My Tech Stuff/View Controllers/LoginViewController.swift
@@ -8,7 +8,21 @@
 
 import UIKit
 
+enum LoginMode: Int {
+	case login
+	case signUp
+
+	static func getType(for value: Int) -> LoginMode {
+		return LoginMode(rawValue: value) ?? .login
+	}
+}
+
 class LoginViewController: UIViewController {
+
+	// FIXME: remove initing here
+	var networkHandler: NetworkHandler? = NetworkHandler()
+
+	var loginMode: LoginMode = .login
 
 	@IBOutlet var usernameTextField: UITextField!
 	@IBOutlet var passwordTextField: UITextField!
@@ -20,12 +34,30 @@ class LoginViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		updateLoginFields()
 	}
 
 	@IBAction func loginTypeChanged(_ sender: UISegmentedControl) {
+		loginMode = LoginMode.getType(for: sender.selectedSegmentIndex)
+		updateLoginFields()
 	}
 
 	@IBAction func loginButtonPressed(_ sender: UIButton) {
+	}
+
+	private func updateLoginFields() {
+		switch loginMode {
+		case .login:
+			confirmPasswordTextField.isHidden = true
+			emailTextField.isHidden = true
+			loginButton.setTitle("Login", for: .normal)
+			loginTypeSelector.selectedSegmentIndex = loginMode.rawValue
+		case .signUp:
+			confirmPasswordTextField.isHidden = false
+			emailTextField.isHidden = false
+			loginButton.setTitle("Sign Up", for: .normal)
+			loginTypeSelector.selectedSegmentIndex = loginMode.rawValue
+		}
 	}
 
 }
