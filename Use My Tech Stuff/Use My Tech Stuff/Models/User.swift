297262fb3b73c2ffd8be803ccc7e229ac61dd3db
Michael Redig <mredig@gmail.com>
2019-05-20 12:02:59 -0500

    added user and bearer structs (not 100% sure they will be used)

---
+++ Use My Tech Stuff/Use My Tech Stuff/Models/User.swift
@@ -0,0 +1,19 @@
+//
+//  User.swift
+//  Use My Tech Stuff
+//
+//  Created by Michael Redig on 5/20/19.
+//  Copyright © 2019 Red_Egg Productions. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+	let username: String
+	let password: String
+	let email: String
+}
+
+struct Bearer {
+	let token: String
+}
--- Use My Tech Stuff/Use My Tech Stuff/View Controllers/ViewController.swift
+++ Use My Tech Stuff/Use My Tech Stuff/View Controllers/ViewController.swift
@@ -10,7 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet var usernameTextField: UITextField!
+	@IBOutlet var passwordTextField: UITextField!
+	@IBOutlet var confirmPasswordTextField: UITextField!
+	@IBOutlet var emailTextField: UITextField!
+
+	@IBOutlet var loginTypeSelector: UISegmentedControl!
+	@IBOutlet var loginButton: UIButton!
+
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 	}
+
+
+	@IBAction func loginTypeChanged(_ sender: UISegmentedControl) {
+	}
+	
+	@IBAction func loginButtonPressed(_ sender: UIButton) {
+	}
+	
 }
