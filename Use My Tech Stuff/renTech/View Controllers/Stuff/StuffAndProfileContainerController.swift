1a8bdbd149b9f60a9c010d2f7e938c0d495a2162
Michael Redig <mredig@gmail.com>
2019-05-23 15:17:16 -0500

    consolidated the profile and stuff view controllers

---
+++ Use My Tech Stuff/renTech/View Controllers/Stuff/StuffAndProfileContainerController.swift
@@ -0,0 +1,27 @@
+//
+//  StuffAndProfileContainerController.swift
+//  renTech
+//
+//  Created by Michael Redig on 5/23/19.
+//  Copyright © 2019 Red_Egg Productions. All rights reserved.
+//
+
+import UIKit
+
+class StuffAndProfileContainerController: UIViewController, TechStuffAccessor {
+
+	var techStuffController: TechStuffController?
+
+	@IBOutlet var profileContainerView: UIView!
+	@IBOutlet var stuffContainerView: UIView!
+
+	override func viewDidLoad() {
+        super.viewDidLoad()
+	}
+
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+		if let dest = segue.destination as? TechStuffAccessor {
+			dest.techStuffController = techStuffController
+		}
+	}
+}
